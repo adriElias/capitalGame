f7b8cebb95ab464598b52ae0d80f466c4593d66f
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-21 13:05:39 +0100

    feat: read countries.txt and load HashMap

--- src/main/java/org/capitalGame/App.java
+++ src/main/java/org/capitalGame/App.java
@@ -5,6 +5,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( " --- Capital Game  --- " );
+        CapitalGame game = new CapitalGame();
+        game.loadFile("assets/countries.txt");
 
     }
 }
---
+++ src/main/java/org/capitalGame/CapitalGame.java
@@ -0,0 +1,35 @@
+package org.capitalGame;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CapitalGame {
+    private Map<String, String> countries = new HashMap<>();
+
+    public CapitalGame() {
+
+    }
+
+    public void loadFile(String filename) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String country = parts[0].trim();
+                String capital = parts[1].trim();
+                countries.put(country, capital);
+            }
+            reader.close();
+
+            System.out.println("Loaded file.");
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
